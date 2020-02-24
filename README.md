0197786ee14432561d90b9b23b383feb405b7522
Dorota-TW <dorota.sobkow@thoughtworks.com>
2020-02-24 16:53:22 +0000

    PRMT-939 [JG/DS] Introduce Terraform bootstrap for GP2GP infra

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# prm-gp2gp-support-infra
+# PRM GP2GP support infrastructure
+
+This is the minimal, very first setup of terraform backend in S3 and DynamoDB.
+
+The terraform state produced in this repository is pushed to a separate bucket with AWS CLI.
+
+## Bootstrapping Terraform
+
+Export your AWS credentials in shell (if you have credentials in `~/.aws/credentials` that will work too):
+```
+export AWS_ACCESS_KEY_ID=***********
+export AWS_SECRET_ACCESS_KEY=**************************
+unset AWS_SESSION_TOKEN
+```
+
+Enter docker container with terraform and AWS CLI by typing:
+```
+dojo
+```
+at the root of this repository.
+
+Assume role with elevated permissions:
+```
+eval $(aws-cli-assumerole -rmfa <role-arn> <mfa-otp-code>)
+```
+
+Run terraform:
+```
+./tasks apply
+```
+
+Push bootstrap state:
+```
+./tasks push
+```
---
+++ s3.tf
@@ -0,0 +1,66 @@
+# S3 bucket to hold terraform states of other repos
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "prm-gp2gp-terraform-state"
+  acl    = "private"
+
+  # To allow rolling back states
+  versioning {
+    enabled = true
+  }
+
+  # To cleanup old states eventually
+  lifecycle_rule {
+    enabled = true
+
+    noncurrent_version_expiration {
+      days = 360
+    }
+  }
+
+  tags = {
+    Name = "Terraform states of GP2GP infrastructure"
+  }
+}
+
+# S3 bucket to hold terraform state produced in this repo
+resource "aws_s3_bucket" "terraform_bootstrap_state" {
+  bucket = "prm-gp2gp-terraform-bootstrap-state"
+  acl    = "private"
+
+  # To allow rolling back states
+  versioning {
+    enabled = true
+  }
+
+  # To cleanup old states eventually
+  lifecycle_rule {
+    enabled = true
+
+    noncurrent_version_expiration {
+      days = 360
+    }
+  }
+
+  tags = {
+    Name = "Terraform state of the prm-gp2gp-support-infra"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state_block" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_bootstrap_state_block" {
+  bucket = aws_s3_bucket.terraform_bootstrap_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
